578f427a06cab339fa446d91e962c51ab7770e53
DucIsMee <tommyminecraft2005@gmail.com>
2026-03-06 21:50:06 +0700

    8Puzzle

--- 8PuzzleMain.py
+++ 8PuzzleMain.py
@@ -36,6 +36,35 @@ def bfs(start, goal):
                     queue.put((new_state, path + [new_state]))
     return None
 
+def dfs(start, goal):
+    stack = [(start, [start])]   
+    visited = {tuple(np.array(start).reshape(-1))}
+
+    while stack:
+        state, path = stack.pop()
+
+        if state == goal:
+            return path
+
+        x, y = [(i, j) for i in range(3) for j in range(3) if state[i][j] == 0][0]
+
+        moves = [(-1,0),(1,0),(0,-1),(0,1)]
+
+        for dx, dy in moves:
+            nx, ny = x + dx, y + dy
+
+            if 0 <= nx < 3 and 0 <= ny < 3:
+                new_state = [row[:] for row in state]
+                new_state[x][y], new_state[nx][ny] = new_state[nx][ny], new_state[x][y]
+
+                flat = tuple(np.array(new_state).reshape(-1))
+
+                if flat not in visited:
+                    visited.add(flat)
+                    stack.append((new_state, path + [new_state]))
+
+    return None
+
 def manhattan(state):
     distance = 0
     for i in range(3):
@@ -84,18 +113,23 @@ def draw_state(screen,font,state):
                 screen.blit(text,(j*TILE_SIZE+TILE_SIZE//3,i*TILE_SIZE+TILE_SIZE//4))
     pygame.display.flip()
 
-def draw_buttons(screen,font):
-    pygame.draw.rect(screen,(0,200,0),(0,300,WIDTH//2,BUTTON_HEIGHT))
-    pygame.draw.rect(screen,(200,0,0),(WIDTH//2,300,WIDTH//2,BUTTON_HEIGHT))
-    pygame.draw.rect(screen,(0,0,200),(2*WIDTH//3,300,WIDTH//3,BUTTON_HEIGHT))
-    
-    bfs_text=font.render("BFS",True,(255,255,255))
-    astar_text=font.render("A*",True,(255,255,255))
-    reset_text=font.render("Reset",True,(255,255,255))
+def draw_buttons(screen, font):
+    button_width = WIDTH // 4
 
-    screen.blit(bfs_text,(WIDTH//6-20,310))
-    screen.blit(astar_text,(3*WIDTH//2-20,310))
-    screen.blit(reset_text,(5*WIDTH//6-30,310))
+    pygame.draw.rect(screen,(0,200,0),(0,300,button_width,BUTTON_HEIGHT))                 
+    pygame.draw.rect(screen,(200,100,0),(button_width,300,button_width,BUTTON_HEIGHT))   
+    pygame.draw.rect(screen,(200,0,0),(2*button_width,300,button_width,BUTTON_HEIGHT))   
+    pygame.draw.rect(screen,(0,0,200),(3*button_width,300,button_width,BUTTON_HEIGHT))    
+
+    bfs_text = font.render("BFS",True,(255,255,255))
+    dfs_text = font.render("DFS",True,(255,255,255))
+    astar_text = font.render("A*",True,(255,255,255))
+    reset_text = font.render("Reset",True,(255,255,255))
+
+    screen.blit(bfs_text,(button_width//2-20,310))
+    screen.blit(dfs_text,(button_width + button_width//2-20,310))
+    screen.blit(astar_text,(2*button_width + button_width//2-20,310))
+    screen.blit(reset_text,(3*button_width + button_width//2-30,310))
 
     pygame.display.flip()
 
@@ -134,6 +168,9 @@ def main():
                         run_solution(screen, font, solution)
                     elif x < 2*WIDTH//3: 
                         solution = astar(start_state)
+                        run_solution(screen, font, solution)
+                    elif x < 2*WIDTH//3:
+                        solution = dfs(start_state, GOAL_STATE)
                         run_solution(screen, font, solution)
                     else:  # Reset
                         current_state = start_state
